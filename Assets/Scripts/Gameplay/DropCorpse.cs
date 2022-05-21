41b283c8f60a595f3ebe0d50cfad12b15c541dec
Kayla Kwock <kwock808@gmail.com>
2022-05-21 02:39:42 -1000

    initial commit

---
+++ Assets/Scripts/Gameplay/DropCorpse.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCorpse : MonoBehaviour
+{
+
+    //PlayerController playerController;
+    //[SerializedField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerController = player.GetComponent<PlayerController>();
+        Debug.Log("Hello World!");
+    }
+}
